ca26334bbac4bbc9611fa92fc0266f639e7dae1f
Eugen Markov <58852894+EugenMarkov@users.noreply.github.com>
2020-04-01 20:57:44 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -19,7 +19,7 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   cache,
-  uri: "https://playergraphql.herokuapp.com/graphql",
+  uri: "/graphql",
   resolvers,
   typeDefs,
 });
