1e5934b3dd6168bd298ec7e2f56c9216c95238c7
Eugen Markov <seroglaziymail@gmail.com>
2020-04-02 20:10:39 +0300

    fix

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://EugenAdmin:MovieMaker123@movieplayer-pjb2f.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "random very very secret string"
+};
